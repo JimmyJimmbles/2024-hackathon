0915b1bfa374ed130a60dc2e64b5d3e3189c1ed4
Jay Jones <jaypjonesofficial@gmail.com>
2024-08-09 16:45:55 -0500

    finalizing portfolio

--- scripts.js
+++ scripts.js
@@ -8,3 +8,30 @@ for (const element of fuckYeahs) {
     alert("YO you clicked my button");
   });
 }
+
+// JavaScript to trigger the glitch effect
+// const glitchText = document.querySelector(".glitch-text");
+
+// setInterval(() => {
+//   glitchText.style.animation = "none";
+//   setTimeout(() => (glitchText.style.animation = ""), 10);
+// }, 3000);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const glitches = document.querySelectorAll(".glitch-text");
+
+  for (const glitch of glitches) {
+    const glitchText = glitch.getAttribute("data-text");
+
+    setInterval(() => {
+      const randomChar = String.fromCharCode(33 + Math.random() * 94);
+      const glitchIndex = Math.floor(Math.random() * glitchText.length);
+      const newText =
+        glitchText.substr(0, glitchIndex) +
+        randomChar +
+        glitchText.substr(glitchIndex + 1);
+      glitch.textContent = newText;
+      setTimeout(() => (glitch.textContent = glitchText), 100);
+    }, 500);
+  }
+});
